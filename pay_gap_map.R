6f04154170e0f2cebe3f46db39b918fab684b7b4
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-05-05 11:05:28 +0200

    Final version of the map

--- pay_gap_map.R
+++ pay_gap_map.R
@@ -2,51 +2,78 @@
 # DON'T TOUCH IT ^^^
 # Link : https://www.datanovia.com/en/blog/how-to-create-a-map-using-ggplot2/
 
-
 #charge the libraries
 library(ggplot2)
 library(tidyverse)
 library(maps)
 library(rworldmap)
+library(dplyr)
 
-# #EU Contries
+#Solution from this video : https://www.youtube.com/watch?v=AgWgPSZ7Gp0
+
+##create a data frame and mixe it with the world df with coordinates
+data <- data.frame(countries = payGapEurope$Country, GDP = payGapEurope$GDP)
+colnames(data)[1]  <- "region"
+
+mapdata <- map_data("world")
+mapdata <- left_join(mapdata, data, by="region")
+
+##filter the NA values
+
+mapdata1 <- mapdata %>% filter(!is.na(mapdata$GDP))
+view(mapdata1)
+
+##creation of the map
+
+map1 <- ggplot(mapdata1, aes (x=long, y = lat, group =group))+
+  geom_polygon(aes(fill = GDP), color = "black")
+map1
+
+map2 <- map1 + scale_fill_gradient(name= "Gender pay gap with GDP", low = "yellow", high = "red", na.value= "grey")+
+  theme(axis.text.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.y = element_blank(),
+        axis_title.x=element_blank(),
+        rect = element_blank())
+map2
+
+# #EU Countries
+# 
 # some.eu.countries <- c(
-#   mapRegion="Europe"
+#   "Portugal", "Spain", "France", "Switzerland", "Germany",
+#   "Austria", "Belgium", "UK", "Netherlands",
+#   "Denmark", "Poland", "Italy", 
+#   "Croatia", "Slovenia", "Hungary", "Slovakia",
+#   "Czech republic","Luxembourg","Greece","Sweden", "Finland","Estonia",
+#   "Lithuania","Lettonia", "Ukraine","Romania", "Bulgaria", "Norway(?!:Svalbard)"
 # )
+
+# # Other code lines
+# countriesISO <- c("FR","UK","BE")
 # 
 # data <- data.frame(countries = payGapEurope$Country, GDP = payGapEurope$GDP)
-# map <- map_data("Europe")
-# k <- ggplot(data, aes(fill = murder))
-#   
-# k + geom_map(aes(map_id = state), map = map) 
-# k + expand_limits(x = map$long, y = map$lat)
+# map <-  map_data(map = "world") %>% filter(region %in% countriesISO)
+# k <- ggplot(data, aes(fill = countries))
+# 
+# k + geom_map(aes(map_id = countries), map = map) +
+#   expand_limits(x = map$long, y = map$lat)
 
-
-
-
-# Some EU Contries
-some.eu.countries <- c(
-  "Portugal", "Spain", "France", "Switzerland", "Germany",
-  "Austria", "Belgium", "UK", "Netherlands",
-  "Denmark", "Poland", "Italy", 
-  "Croatia", "Slovenia", "Hungary", "Slovakia",
-  "Czech republic","Luxembourg"
-)
-
-# Retrieve the map data
-some.eu.maps <- map_data("world", region = some.eu.countries)
-
-# Compute the centroid as the mean longitude and lattitude
-# Used as label coordinate for country's names
-region.lab.data <- some.eu.maps %>%
-  group_by(region) %>%
-  summarise(long = mean(long), lat = mean(lat))
-
-ggplot(some.eu.maps, aes(x = long, y = lat)) +
-  geom_polygon(aes( group = group, fill = region))+
-  # geom_text(aes(label = region), data = region.lab.data,  size = 3, hjust = 0.5)+
-  scale_fill_viridis_d()+
-  theme_void()+
-  theme(legend.position = "none")
-
-
+# 
+# # Retrieve the map data
+# some.eu.maps <- map_data("world", region = some.eu.countries)
+# 
+# # Compute the centroid as the mean longitude and lattitude
+# # Used as label coordinate for country's names
+# region.lab.data <- some.eu.maps %>%
+#   group_by(region) %>%
+#   summarise(long = mean(long), lat = mean(lat))
+# 
+# ggplot(some.eu.maps, aes(x = long, y = lat)) +
+#   geom_polygon(aes( group = group, fill = region))+
+#   # geom_text(aes(label = region), data = region.lab.data,  size = 3, hjust = 0.5)+
+#   scale_fill_viridis_d()+
+#   theme_void()+
+#   theme(legend.position = "none")
+# 
+# 
