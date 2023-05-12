5bdb177916247581979845adfb1b3afc8b7c7c8e
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-05-12 18:52:51 +0200

    somme add-ons : changing the label, alphabetic order for countries

--- shinymap/app.R
+++ shinymap/app.R
@@ -12,29 +12,30 @@ library(tidyverse)
 library(ggplot2)
 library(maps)
 library(rworldmap)
-library(leaflet)
+library(hexbin)
 
 
 #Load datasets
 
 data <- read_csv("pay_gap_Europe.csv", show_col_types = FALSE)
-colnames(data)[1]  <- "region"
-colnames(data)[2]  <- "year"
+colnames(data) <- c("region","year","GDP","Urban population","Industry","Business","Mining", 
+                  "Manufacturing","Electricity supply","Water supply","Construction",
+                  "Retail","Transportation","Accommodation","Information",
+                  "Financial","Real estate","Science",
+                  "Administrative","Public administration","Education",
+                  "Health","Arts","Other")
+
+# colnames(data)[1]  <- "region"
+# colnames(data)[2]  <- "year"
 worldmap <- map_data("world")
 mapdata <- left_join(worldmap, data, by="region", relationship = "many-to-many")
 mapdata <- mapdata %>% filter(!is.na(mapdata$GDP))
-colname = c("GDP","Industry","Business","Mining", 
-            "Manufacturing","Electricity_supply","Water_supply","Construction",
-            "Retail trade","Transportation","Accommodation","Information",
-            "Financial","Real estate","Professional_scientific",
-            "Administrative","Public_administration","Education",
-            "Human_health","Arts","Other")
-
-# listEurope <- read_csv("european.csv", show_col_types = FALSE)
-# colnames(listEurope)[1] <- "region"
-# listEurope1 <- semi_join(worldmap, listEurope, by="region")
-# 
-# mapdata <- rbind.fill(listEurope1, mapdata)
+selectedcolumn = c("GDP","Industry","Business","Mining", 
+                "Manufacturing","Electricity supply","Water supply","Construction",
+                "Retail","Transportation","Accommodation","Information",
+                "Financial","Real estate","Science",
+                "Administrative","Public administration","Education",
+                "Health","Arts","Other")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -48,7 +49,7 @@ ui <- fluidPage(
                   value = 2010, step = 2, sep = "",
                   animate = TRUE),
       selectInput("jobField", "Choose the Job field :",
-                  choices=colname),
+                  choices=selectedcolumn),
       tableOutput("view"),
     ),
     
@@ -63,12 +64,12 @@ ui <- fluidPage(
 server <- function(input, output) {
   
       output$distPlot <- renderPlot({
-        
           year_map <- subset(mapdata, year==input$yearCursor)
           data <- select(year_map,long,lat,group,order,region,subregion,year,input$jobField)
+          
           map <- ggplot(data, aes(x = long, y = lat, group = group)) +
             geom_polygon(aes(fill = !!sym(input$jobField)), color = "gray", linetype=1) +
-            scale_fill_viridis_c(alpha = 0.75)+
+            scale_fill_viridis_c(alpha = 0.75, option="A")+
             theme(
               axis.text.x = element_blank(),
               axis.text.y = element_blank(),
@@ -82,11 +83,12 @@ server <- function(input, output) {
           },width=800,height=950)
       
       output$view <- renderTable({
-        year_map1 <- subset(mapdata, year==input$yearCursor)
-        year_map1 = subset(year_map1, select = -c(long,lat,group,order,subregion))
+        year_map1 <- subset(mapdata, year==input$yearCursor,select = -c(long,lat,group,order,subregion))
         year_map1 <- year_map1 %>% distinct(.keep_all=TRUE)
         data1 <- select(year_map1,region,input$jobField)
-        head(data1, n=20)
+        data1 <- data1[order(data1$region),]
+        
+        head(data1, n=27)
       })
     
 }
