fb63ca5e7df3dabc64b94b2274d2785a487f8309
Marion SALAUN <marion.salauen.001@student.uni.lu>
2023-05-27 18:15:26 +0200

    Matrix added to shiny app

--- shinymap/app.R
+++ shinymap/app.R
@@ -14,9 +14,14 @@ library(maps)
 library(rworldmap)
 library(shinydashboard)
 library(reshape2)
+library(RColorBrewer)
+library(hexbin)
+library(bslib) # for convenient window arrangement in plotting
+library(gridExtra)
 
 #Load datasets
 
+## -- FOR THE MAP
 data <- read_csv("pay_gap_Europe.csv", show_col_types = FALSE)
 colnames(data) <- c("region","year","GDP","Urban population","Industry","Business","Mining", 
                   "Manufacturing","Electricity supply","Water supply","Construction",
@@ -27,11 +32,16 @@ colnames(data) <- c("region","year","GDP","Urban population","Industry","Busines
 
 worldmap <- map_data("world")
 #glimpse(data)
-
 mapdata <- left_join(data, worldmap, by="region", relationship = "many-to-many")
 
-allcolumnNames = c(colnames(data))
+## -- SET THE DIFFERENT VARIABLES
 
+central_europe <- c("France","Luxembourg","Netherlands","Belgium","Germany", "Austria", "Czech Republic", "Hungary", "Poland", "Slovakia", "Slovenia")
+northern_europe <- c("Denmark", "Estonia", "Finland", "Iceland", "Latvia", "Lithuania", "Norway", "Sweden")
+eastern_europe <- c("Bulgaria", "Croatia", "Romania")
+southern_europe <- c("Cyprus", "Greece", "Italy", "Malta", "Portugal", "Spain")
+
+allcolumnNames = c(colnames(data))
 selectedcolumn = c("GDP","Industry","Business","Mining", 
                 "Manufacturing","Electricity supply","Water supply","Construction",
                 "Retail","Transportation","Accommodation","Information",
@@ -40,6 +50,10 @@ selectedcolumn = c("GDP","Industry","Business","Mining",
                 "Health","Arts","Other")
 
 selectedcountries = c(unique(data$region))
+
+## -- FOR THE MATRIX
+
+pay_gap_Europe <- read.csv("pay_gap_Europe.csv")
 
 # Header ----
 header <- dashboardHeader(title="Gender Pay Gap")
@@ -55,18 +69,25 @@ sidebar <- dashboardSidebar(
                    selectInput("jobField", "Choose the Job field :",
                                choices=selectedcolumn),
   ),
-  
   conditionalPanel(condition="input.tabselected==2",
+                   actionButton("resetButton", "Reset"),
+                   actionButton("allofthemButton", "Select all countries"),
                    checkboxGroupInput("country","Select Country:",
                                       choices = selectedcountries,
-                                      selected = c("France"),
-                   ),
+                                      selected = c("France"),),
   ),
   conditionalPanel(condition="input.tabselected==3",
                    selectInput("variables","Select variables:",
                                       choices = allcolumnNames,
                                       selected = c("region"),
                                       multiple=TRUE,
+                   ),
+  ),
+  conditionalPanel(condition="input.tabselected==4",
+                   selectInput("variables","Select variables:",
+                               choices = allcolumnNames,
+                               selected = c("region"),
+                               multiple=TRUE,
                    ),
   )
 )
@@ -76,17 +97,21 @@ body <- dashboardBody(
   mainPanel(
     tabsetPanel(
       tabPanel("Map", value=1,
-                 fluidRow(
-                   box(title = "Map of Europe", height = 650,width = 7, plotOutput("distPlot")),
-                   box(width = 5, height = 650, tableOutput("viewCountries")),
-                 )
+                   fluidRow(
+                     box(title = "Map of Europe", height = 650,width = 7, plotOutput("distPlot")),
+                     box(width = 5, height = 650, tableOutput("viewCountries")),
+                   )
                ),
       tabPanel("Plot", value=2,
-               fluidRow(
-                   box(width = 12,plotOutput("linePlot")),
-                )
+                  plotOutput("linePlot"),
               ),
-      tabPanel("Data", value=3,
+      tabPanel("Correlation", value=3, 
+                  fluidRow(
+                    box(width=6, height = 500, plotOutput("corrMatrix")),
+                    box(width=6, height = 500, plotOutput("boxPlot"))
+                  )
+               ),
+      tabPanel("Data", value=4,
                tableOutput("distData")),
       id = "tabselected"
     )
@@ -103,7 +128,6 @@ body <- dashboardBody(
                                 ')))
 )
 ui <- dashboardPage(header, sidebar, body, skin='purple')
-
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -168,16 +192,67 @@ server <- function(input, output) {
           facet_wrap(vars(region), ncol=5) +
           theme_bw()
 
-      },width=500,height=400)
+      },width=1000,height=800)
+      
+      #Correlation matrix
+      output$corrMatrix <- renderPlot({
+        
+        #create a new column with the country
+        pay_gap_Europe$Country_factor <- as.factor(pay_gap_Europe$Country)
+        #give to each country a qualitative value
+        pay_gap_Europe$Country_numeric <- as.numeric(pay_gap_Europe$Country_factor) - 1
+        pay_gap_Europe
+        
+        #remove from the past dataset the qualitative values
+        pay_gap<-subset(pay_gap_Europe,select=-c(Country, Country_factor) )
+        pay_gap
+        
+        #see the correlation of pay_gap
+        pay_gap.corr<-cor(na.omit(pay_gap))
+        pay_gap.corr
+        
+        #Make a graph for the correlation
+        palette <- colorRampPalette(brewer.pal(8, "PuOr"))(25)
+        
+        heatmap(x = pay_gap.corr,
+                # Colv = NA,
+                # Rowv = NA,
+                cexRow = 0.8,
+                cexCol = 0.8,
+                labCol = allcolumnNames,
+                labRow = allcolumnNames,
+                col= palette,
+                # scale="column", # scale allow the normalization
+                symm = TRUE)
+        legend(x = "bottomright", c("Max", "Mid","Min"), fill = c("Purple","Beige","Brown"))
+        
+      })
+      
+      #Boxplot
+      output$boxPlot <- renderPlot({
+        
+        dataAvg <- read_csv("pay_gap_Europe.csv", show_col_types = FALSE)
+        dataAvg <- transform(dataAvg, subgroup = case_when(
+          Country %in% central_europe ~ "Central Europe",
+          Country %in% northern_europe ~ "Northern Europe",
+          Country %in% eastern_europe ~ "Eastern Europe",
+          Country %in% southern_europe ~ "Southern Europe",
+          TRUE ~ "Other"
+        ))
+        
+        p1 <- ggplot(dataAvg, aes(x=dataAvg$Average, y=Urban_population)) + geom_point(aes(col=subgroup))
+        p2 <- ggplot(dataAvg, aes(x=subgroup, y=GDP, fill=subgroup)) + geom_boxplot()
+        p3 <- ggplot(dataAvg, aes(x=subgroup, y=Average, fill=subgroup)) + geom_boxplot()
+        grid.arrange(p2, p3, nrow = 1, widths = c(1, 1))
+        par(mfrow=c(1,1))
+      
+      })
       
       #show dataset
       output$distData <- renderTable(
         data %>%
           dplyr::select(unlist(input$variables, use.names = FALSE))
       )
-      
-      
-    
 }
 
 # Run the application 
